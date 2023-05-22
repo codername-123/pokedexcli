c4c4f4272fff236e85e8d88af753673766be183f
codername-132 <nitin.rawat629@gmail.com>
2023-05-22 16:29:39 +0530

    Implemented LocationAreaRequest function that makes get request to /location-area endpoint of pokemon api url
    Added type to handle the unmarshelled response of location area endpoint request call

---
+++ internal/pokeapi/location_area.go
@@ -0,0 +1,36 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c Client) LocationAreaRequest(pageUrl *string) (ResponseLocationArea, error) {
+	url := baseUrl + "/location-area"
+	if pageUrl != nil {
+		url = *pageUrl
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return ResponseLocationArea{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return ResponseLocationArea{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ResponseLocationArea{}, err
+	}
+
+	var locationResp ResponseLocationArea
+	err = json.Unmarshal(body, &locationResp)
+	if err != nil {
+		return ResponseLocationArea{}, err
+	}
+	return locationResp, nil
+}
