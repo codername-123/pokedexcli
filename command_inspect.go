e4c75277ef5674753e9b707ae30e800a72d1ebba
codername-132 <nitin.rawat629@gmail.com>
2023-05-25 13:02:14 +0530

    Implement inspect command

--- command_inspect.go
+++ command_inspect.go
@@ -1,6 +1,31 @@
 package main
 
-// TODO: implement this function
+import (
+	"errors"
+	"fmt"
+)
+
 func commandInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("pokemon is not specified check help")
+	}
+	pokemon, ok := cfg.caughtPokemon[args[0]]
+	if !ok {
+		return errors.New("you have not caught the pokemon")
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+
+	fmt.Println("Stats: ")
+	for _, s := range pokemon.Stats {
+		fmt.Printf(" -%s: %d\n", s.Stat.Name, s.BaseStat)
+	}
+	fmt.Println("Types: ")
+	for _, t := range pokemon.Types {
+		fmt.Printf(" -%s\n", t.Type.Name)
+	}
+
 	return nil
 }
