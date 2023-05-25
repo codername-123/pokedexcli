9a00d973384a9bf15717a68186901555e49f29f7
codername-132 <nitin.rawat629@gmail.com>
2023-05-25 12:22:34 +0530

    change formatting of how result is printed

--- command_explore.go
+++ command_explore.go
@@ -6,13 +6,15 @@ import (
 )
 
 func commandExplore(cfg *config, args ...string) error {
-	if len(args) != 0 {
+	if len(args) != 1 {
 		return errors.New("city name not specified check help")
 	}
+	fmt.Printf("Exploring %s...\n", args[0])
 	pokemonReponse, err := cfg.pokeClient.ListPokemons(args[0])
 	if err != nil {
 		return err
 	}
+	fmt.Println("Found Pokemon:")
 	for _, pokemon := range pokemonReponse.PokemonEncounters {
 		fmt.Print(" - ")
 		fmt.Println(pokemon.Pokemon.Name)
