62ffec6f1772717b0285d66adcde05c8ce60d870
codername-132 <nitin.rawat629@gmail.com>
2023-05-25 13:18:38 +0530

    Implement pokdex command

--- command_pokedex.go
+++ command_pokedex.go
@@ -1,6 +1,14 @@
 package main
 
-// TODO: implement the function
+import (
+	"fmt"
+)
+
 func commandPokedex(cfg *config, args ...string) error {
+
+	fmt.Println("Your Pokedex:")
+	for _, v := range cfg.caughtPokemon {
+		fmt.Printf(" - %s\n", v.Name)
+	}
 	return nil
 }
